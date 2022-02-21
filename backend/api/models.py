c4de3c452b96e6b521adefbd32147d0336831a97
dstdbsgml <53514545+dst03106@users.noreply.github.com>
2022-02-21 20:56:14 +0900

    REFACTOR: login with email not username

--- backend/api/models.py
+++ backend/api/models.py
@@ -26,9 +26,3 @@ def create_user_profile(sender, instance, created, **kwargs):
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
     
-# class Account(models.Model):
-#     id = models.AutoField(primary_key=True)
-#     name = models.CharField(max_length = 50)
-#     email = models.CharField(max_length = 200)
-#     password = models.CharField(max_length = 200)
-#     created_at = models.DateTimeField(auto_now_add=True)
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,9 +8,3 @@ urlpatterns = [
     path("auth/user/", UserAPI.as_view()),
     path("auth/profile/<int:user_pk>/update/", ProfileUpdateAPI.as_view()),
 ]
-# urlpatterns = [
-#     path('accounts', views.account_list.as_view()),
-#     path('accounts/<int:pk>', views.account.as_view()),
-#     path('login', views.login.as_view()),
-#     path('auth', include('rest_framework.urls', namespace='rest_framework'))
-# ]
