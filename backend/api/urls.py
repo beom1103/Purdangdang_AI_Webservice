130cd729f219559ad1d028be4ccda126f645feaf
dstdbsgml <53514545+dst03106@users.noreply.github.com>
2022-02-22 12:31:32 +0900

    FEAT: add logout

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 # from account import views
 from .views import RegistrationAPI, LoginAPI, UserAPI, ProfileUpdateAPI
+from knox import views as knox_views
 
 urlpatterns = [
     path("auth/register/", RegistrationAPI.as_view()),
     path("auth/login/", LoginAPI.as_view()),
+    path('auth/logout',knox_views.LogoutView.as_view(), name="knox-logout"),
     path("auth/user/", UserAPI.as_view()),
     path("auth/profile/<int:user_pk>/update/", ProfileUpdateAPI.as_view()),
 ]
