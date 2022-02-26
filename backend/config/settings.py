56a949a66c0a6b66a675d57ac7159e2390eaf59f
Yun Hee <dstdbsgml@naver.com>
2022-02-26 20:59:49 +0900

    Merge branch 'feature_back' into feature_front

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -70,15 +70,16 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_WHITELIST = [
-    'http://0.0.0.0:8000',
-    'http://0.0.0.0:3000',
-    'http://127.0.0.1:8000',
-    'http://127.0.0.1:3000',
-    'http://localhost:8000',
-    'http://localhost:3000',   
-] # Scripts 안에서 리소스 요청을 허용할 도메인 추가
+# CORS_ORIGIN_WHITELIST = [
+#     'http://0.0.0.0:8000',
+#     'http://0.0.0.0:3000',
+#     'http://127.0.0.1:8000',
+#     'http://127.0.0.1:3000',
+#     'http://localhost:8000',
+#     'http://localhost:3000',   
+# ] # Scripts 안에서 리소스 요청을 허용할 도메인 추가
 
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'config.urls'
