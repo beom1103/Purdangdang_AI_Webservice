b1dd75a277b9b839b0a34d1f312f6a0be9692f1c
gkgkgkgkdlel <gkgkgkgkdlel@naver.com>
2022-03-02 01:47:08 +0900

    필요없는 파일 정리

--- backend/login/urls.py
+++
@@ -1,7 +0,0 @@
-from urllib.parse import urlparse
-from django.urls import  path
-from . import views
-
-urlpatterns = [
-    path('', views.index),
-]
--- backend/login/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def index(request):
-    return HttpResponse("안녕하세요 login에 오신것을 환영합니다.")
