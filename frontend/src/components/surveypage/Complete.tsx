f976ad33d8774115dbae8ead43dc03091662cf0d
3기-1반 애범 <gogot1103@naver.com>
2022-03-09 11:13:21 +0900

    FEAT : 식물이미지 검색 및 질병 api로직 수정

--- frontend/src/components/surveypage/Complete.tsx
+++ frontend/src/components/surveypage/Complete.tsx
@@ -1,21 +1,89 @@
 import tw from 'tailwind-styled-components';
 import { QuestionBox, Title } from '../../pages/SurveyPage';
 import { User } from '../../store/type';
+import fake from '../../store/fake.json';
+import { useEffect, useRef, useState } from 'react';
 
 type CompleteProps = {
   user: User | undefined;
 };
 
-const Complete: React.FC<CompleteProps> = ({ user }) => {
-  return (
-    <QuestionBox className="animate-fade-in-up">
-      <Title>검사를 완료하였습니다.</Title>
+let count = 0;
+let slideInterval: any;
 
-      <Title>
-        <Green>{user?.username}</Green>님께 추천하는 푸르댕댕이는 다음과
-        같습니다.
-      </Title>
-    </QuestionBox>
+const Complete: React.FC<CompleteProps> = ({ user }) => {
+  const TOTAL_SLIDES = fake.length;
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slideRef = useRef<any>(null);
+
+  const nextSlide = () => {
+    count = (count + 1) % TOTAL_SLIDES;
+    setCurrentSlide(count);
+    slideRef !== null && slideRef?.current.classList.add('animate-fade-in-up');
+  };
+
+  const prevSlide = () => {
+    count = (count - 1) % TOTAL_SLIDES;
+    setCurrentSlide(count);
+    slideRef.current.classList.add('animate-fade-in-up');
+  };
+
+  const startSlider = () => {
+    slideInterval = setInterval(() => {
+      nextSlide();
+    }, 3000);
+  };
+
+  const pauseSlider = () => {
+    clearInterval(slideInterval);
+  };
+
+  const removeAnimation = () => {
+    slideRef !== null &&
+      slideRef.current.classList.remove('animate-fade-in-up');
+  };
+
+  useEffect(() => {
+    slideRef.current?.addEventListener('animationend', removeAnimation);
+
+    slideRef.current?.addEventListener('mouseenter', pauseSlider);
+
+    slideRef.current?.addEventListener('mouseleave', startSlider);
+
+    startSlider();
+    return () => {
+      pauseSlider();
+    };
+  }, []);
+
+  return (
+    <div>
+      <QuestionBox className="animate-fade-in-up">
+        <Title>검사를 완료하였습니다.</Title>
+        <Title>
+          <Green>{user?.username}</Green>님께 추천하는 푸르댕댕이는 다음과
+          같습니다.
+        </Title>
+
+        <div className="max-w-screen-xl m-auto">
+          <div ref={slideRef} className="relative w-full select-none">
+            {/* {fake.map((idx: any) => {
+              <div className="aspect-w-16 aspect-hs-9">
+                <img src={idx[currentSlide].image_url} alt="s" />
+                <img src={idx[currentSlide].image_url} alt="s" />
+                <img src={idx[currentSlide].image_url} alt="s" />
+              </div>;
+            })} */}
+            <Img src={fake[currentSlide].image_url} alt="s" />
+
+            <div className="absolute justify-between w-full px-2 transform -translate-y-1/2 top-1/2 center">
+              <button onClick={nextSlide}>이전</button>
+              <button onClick={prevSlide}>다음</button>
+            </div>
+          </div>
+        </div>
+      </QuestionBox>
+    </div>
   );
 };
 
@@ -23,4 +91,9 @@ export default Complete;
 
 const Green = tw.span` 
   text-green-500
+`;
+
+const Img = tw.img` 
+  ease-out
+  delay-500
 `;
